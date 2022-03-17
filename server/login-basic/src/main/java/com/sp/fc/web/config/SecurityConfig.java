3d234c98380fc77c4cc18ea3c02b717bf10bac08
sangeon22 <tkddjsdl33@hanmail.net>
2022-03-17 23:35:08 +0900

    basic-login 실습

--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,21 +1,58 @@
 package com.sp.fc.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("admin")
+                                .password("2222")
+                                .roles("ADMIN")
+                );
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(request->{
                     request
-                            .antMatchers("/**").permitAll()
+                            .antMatchers("/").permitAll()
+                            .anyRequest().authenticated()
                             ;
                 })
+                .formLogin(
+                        login-> login.loginPage("/login")
+                                .permitAll()
+                                .defaultSuccessUrl("/",false)
+                )
                 ;
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
+    }
+
 }
