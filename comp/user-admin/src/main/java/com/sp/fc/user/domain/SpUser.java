2194452edc27385744e155acac4a412c7a0e5385
sangeon22 <tkddjsdl33@hanmail.net>
2022-03-29 20:26:46 +0900

    Spring Security - DaoAuthenticationProvider, UserDetailsService

---
+++ comp/user-admin/src/main/java/com/sp/fc/user/domain/SpUser.java
@@ -0,0 +1,61 @@
+package com.sp.fc.user.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "sp_user")
+public class SpUser implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "user_id"))
+    private Set<SpAuthority> authorities;
+
+    private String email;
+    private String password;
+    private boolean enabled;
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return enabled;
+    }
+
+}
