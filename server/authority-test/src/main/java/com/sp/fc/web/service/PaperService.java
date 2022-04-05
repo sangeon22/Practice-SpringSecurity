e25dee4e83afe741544da9bbae240dc5774af945
sangeon22 <tkddjsdl33@hanmail.net>
2022-04-05 23:30:39 +0900

    Spring Security - Method 후처리, Secured 기반 권한체크, runAs 임시권한부여

--- server/authority-test/src/main/java/com/sp/fc/web/service/PaperService.java
+++ server/authority-test/src/main/java/com/sp/fc/web/service/PaperService.java
@@ -1,6 +1,9 @@
 package com.sp.fc.web.service;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -17,17 +20,25 @@ public class PaperService implements InitializingBean {
 
     }
 
-    public void setPaper(Paper paper){
+    public void setPaper(Paper paper) {
         paperDB.put(paper.getPaperId(), paper);
     }
 
+    @PostFilter("notPrepareState(filterObject)")
     public List<Paper> getMyPapers(String username) {
+//        return paperDB.values().stream().collect(Collectors.toList());
         return paperDB.values().stream().filter(
                 paper -> paper.getStudentIds().contains(username)
         ).collect(Collectors.toList());
     }
 
+    //    @PostAuthorize("returnObject.studentIds.contains(#principal.username)")
     public Paper getPaper(Long paperId) {
         return paperDB.get(paperId);
+    }
+
+    @Secured({"ROLE_PRIMARY", "ROLE_RUN_AS_PRIMARY"})
+    public List<Paper> getAllPapers() {
+        return paperDB.values().stream().collect(Collectors.toList());
     }
 }
--- server/authority-test/src/test/java/com/sp/fc/web/test/PaperTest.java
+++ server/authority-test/src/test/java/com/sp/fc/web/test/PaperTest.java
@@ -35,14 +35,23 @@ public class PaperTest extends WebIntegrationTest{
             .title("시험지2")
             .tutorId("tutor1")
             .studentIds(List.of("user2"))
-            .state(Paper.State.PREPARE)
+            .state(Paper.State.READY)
             .build();
 
+    private Paper paper3 = Paper.builder()
+            .paperId(3L)
+            .title("시험지3")
+            .tutorId("tutor1")
+            .studentIds(List.of("user1"))
+            .state(Paper.State.READY)
+            .build();
 
     @DisplayName("1. user1이 시험지 리스트 조회한다. ")
     @Test
     void test_1(){
         paperService.setPaper(paper1);
+        paperService.setPaper(paper2);
+        paperService.setPaper(paper3);
 
         client = new TestRestTemplate("user1", "1111");
         ResponseEntity<List<Paper>> response = client.exchange(uri("/paper/mypapers"),
@@ -50,6 +59,7 @@ public class PaperTest extends WebIntegrationTest{
                 });
 
         assertEquals(200, response.getStatusCodeValue());
+        assertEquals(1, response.getBody().size());
         System.out.println(response.getBody());
 
     }
@@ -78,4 +88,39 @@ public class PaperTest extends WebIntegrationTest{
         assertEquals(403, response.getStatusCodeValue());
     }
 
+    @DisplayName("5. 교장선생님은 모든 시험지를 볼 수 있다. ")
+    @Test
+    void test_5(){
+        paperService.setPaper(paper1);
+        paperService.setPaper(paper2);
+        paperService.setPaper(paper3);
+
+        client = new TestRestTemplate("primary", "1111");
+        ResponseEntity<List<Paper>> response = client.exchange(uri("/paper/getPapersByPrimary"),
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Paper>>() {
+                });
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(3, response.getBody().size());
+        System.out.println(response.getBody());
+
+    }
+
+    @DisplayName("6. 사용자 user1이 임시로 교장선생님 권한을 얻어서 시험지를 가져온다. ")
+    @Test
+    void test_6(){
+        paperService.setPaper(paper1);
+        paperService.setPaper(paper2);
+        paperService.setPaper(paper3);
+
+        client = new TestRestTemplate("user1", "1111");
+        ResponseEntity<List<Paper>> response = client.exchange(uri("/paper/allpapers"),
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Paper>>() {
+                });
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(3, response.getBody().size());
+        System.out.println(response.getBody());
+
+    }
 }
